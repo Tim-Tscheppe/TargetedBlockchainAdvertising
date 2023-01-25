520ff09f8afe597a32f834029b764982fc02e3e9
Tim-Tscheppe <timothy.tscheppe@marquette.edu>
2023-01-25 10:25:21 -0600

    Added boilerplate code for what the intitial screen can look like

--- Main/Main.py
+++ Main/Main.py
@@ -1,12 +1,31 @@
-import tkinter as tk
-from tkinter import simpledialog
+from tkinter import *
+from functools import partial
 
-ROOT = tk.Tk()
+# Here we can put sender / reciever / amount
+def validateLogin(sender, password):
+	print("Sender :", sender.get())
+	print("password entered :", password.get())
+	return
 
-ROOT.withdraw()
-# the input dialog
-USER_INP = simpledialog.askstring(title="Test",
-                                  prompt="What's your Name?:")
+#window
+tkWindow = Tk()  
+tkWindow.geometry('400x150')  
+tkWindow.title('Tkinter Login Form - pythonexamples.org')
 
-# check it out
-print("Hello", USER_INP)
+# sender label and text entry box
+usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)  
+
+#password label and password entry box
+passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)  
+password = StringVar()
+passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)  
+
+validateLogin = partial(validateLogin, username, password)
+
+#login button
+loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)  
+
+# Get rid of this eventually
+tkWindow.mainloop()
