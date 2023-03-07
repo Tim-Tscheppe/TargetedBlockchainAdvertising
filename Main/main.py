354d94f6dea6a2bde956c27fd37f9f2dd7d4d24b
Tim-Tscheppe <timothy.tscheppe@marquette.edu>
2023-03-06 19:29:05 -0600

    added example of how we can modify flask frontend boilerplate code so we can use it for our purposes:

--- Main/main.py
+++ Main/main.py
@@ -4,20 +4,19 @@ from flask import Flask, request, render_template
 # Flask constructor
 app = Flask(__name__)  
  
-# A decorator used to tell the application
-# which URL is associated function
 @app.route('/', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
-       global first_name
-       first_name = request.form.get("fname")
+       global sender
+       sender = request.form.get("sender")
        global last_name
        last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
+       return "Your name is "+ sender + last_name
     return render_template("form.html")
  
 if __name__=='__main__':
    app.run()
 
-print(first_name)
-print(last_name)
+   # Print initial sender and receiver
+   print("Sender address: " + sender + "\n")
+   print("Receiver address: " + last_name + "\n")
