d4c7be5d4d54e8ac0b7a1e57fc1f5dee3e8c383c
DavidT212 <79224138+DavidT212@users.noreply.github.com>
2023-04-28 16:46:57 -0500

    Add files via upload
    Adding contract deployment

---
+++ Main/SDS/SeleniumProject/src/test/java/test/FirstTest.java
@@ -0,0 +1,99 @@
+package test;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+	
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+        
+		//System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(options);
+    
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		
+		
+		//RemixIDE
+		driver.get("https://remix.ethereum.org/#lang=en&optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.18+commit.87f61d96.js");
+		
+		Actions act = new Actions(driver);
+		Thread.sleep(4000);
+		//add full screen command
+		act.moveByOffset(1, 1).click().build().perform();
+		Thread.sleep(1000);
+		
+		driver.findElement(By.id("uploadFile")).click();
+		Thread.sleep(1000);
+		
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_P);
+		r.keyRelease(KeyEvent.VK_P);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(2000);
+		act.moveByOffset(150, 220).click().build().perform();
+		
+		//Compiling the contract
+		driver.findElement(By.id("verticalIconsKindsolidity")).click();
+		driver.findElement(By.id("compileAndRunBtn")).click();
+		Thread.sleep(10000);
+		
+		
+		//create loops to do this more efficiently and more neatly
+		//Going to Deploy page
+		driver.findElement(By.id("verticalIconsKindudapp")).click();
+		//drop down for Environment
+		driver.findElement(By.id("selectExEnvOptions")).click();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		//Drop down for Coin type
+		driver.findElement(By.id("unit")).click();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		
+		Thread.sleep(500);
+		driver.findElement(By.id("value")).sendKeys("5");
+		//driver.findElement(By.className("form-control")).sendKeys("hello, world");
+
+		
+		driver.findElement(By.xpath("//*[@id=\"runTabView\"]/div/div[2]/div[3]/div[1]/div/div[1]/div[1]/input")).sendKeys("0xc0ffee254729296a45a3885639AC7E10F9d54979,10");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//*[@id=\"runTabView\"]/div/div[2]/div[3]/div[1]/div/div[1]/div[1]/button")).click();
+		driver.findElement(By.xpath("//*[@id=\"instance0xd9145CCE52D386f254917e481eB44e9943F39138\"]/div[1]/span")).click();
+
+		
+	}
+
+}
+//*[@id="runTabView"]/div/div[2]/div[3]/div[1]/div/div[1]/div[1]/button
+//*[@id="instance0xd9145CCE52D386f254917e481eB44e9943F39138"]/div[1]/div/div[1]/span
---
+++ Main/SDS/p.sol
@@ -0,0 +1,44 @@
+// Solidity Implementation of Payment Execution Function
+// @ Tim Tscheppe, Marquette University, 02-19-2023
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.9;
+
+contract p {
+
+    address payable public sender;
+    address payable public receiver;
+    uint public amount;
+    uint public funding;
+
+    // Here is a couple random ETH addresses we can use tests:
+    // 0x71C7656EC7ab88b098defB751B7401B5f6d8976F
+    // 0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E
+    // 0xc0ffee254729296a45a3885639AC7E10F9d54979
+    
+    constructor(address payable _receiver,uint _amount) public payable {
+        sender = msg.sender;
+        receiver = _receiver;
+        amount = _amount;
+        funding = msg.value;
+    }
+
+    receive() external payable {
+        funding += msg.value; 
+    }
+
+    function NewTransaction(address payable _receiver, uint _amount) public payable {
+        funding = msg.value;
+        receiver = _receiver;
+        amount = _amount;
+    }
+
+    function GetBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function ExecutePayment() public payable {
+        receiver.transfer(amount);
+    }
+
+}
